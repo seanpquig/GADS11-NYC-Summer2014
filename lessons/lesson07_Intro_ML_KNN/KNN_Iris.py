c469662193310170d5c09d5be951b2425234d374
Patrick McNamara <patrick.mcnamara86@gmail.com>
2014-06-24 20:07:02 -0400

    fixed KNN_Iris scipt

--- lessons/lesson07_Intro_ML_KNN/KNN_Iris.py
+++ lessons/lesson07_Intro_ML_KNN/KNN_Iris.py
@@ -10,17 +10,17 @@ data['species'] = iris.target
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 
-neighbors_clf = KNeighborsClassifier()
-neighbors_clf.fit(df[iris.feature_names], df.species)
+neighbors_clf = KNeighborsClassifier(n_neighbors = 2)
+neighbors_clf.fit(data[iris.feature_names], data.species)
 neighbors_clf.predict(iris.data)
-pd.crosstab(data.species, neighbors_clf.predict(data[iris.feature_names]))
+pd.crosstab(data['species'], neighbors_clf.predict(data[iris.feature_names]))
 
 from sklearn.metrics import accuracy_score
 y_pred = neighbors_clf.predict(iris.data)
-y_true = df.species
+y_true = data['species']
 
-accuracy_score(y_true, y_pred, normalize=False)
 accuracy_score(y_true, y_pred)
+accuracy_score(y_true, y_pred, normalize=False)
 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_true, y_pred)
